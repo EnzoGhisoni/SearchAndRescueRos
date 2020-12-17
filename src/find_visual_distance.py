1a2ee1309165705b3bc6e000548235081fac6dd5
MaxRobot <enzo.ghisoni@hotmail.fr>
2020-12-17 13:14:28 +0000

    Clean the structure of find_visual_distance.py Add the program generate_image_from_odom_occupancy.py

--- src/find_visual_distance.py
+++ src/find_visual_distance.py
@@ -18,6 +18,7 @@ from sensor_msgs.msg import LaserScan
 from PySide2.QtGui import QTransform
 from PySide2.QtCore import QPointF
 from math import *
+
 """
 Run the camera view
 rosrun image_view image_view image:=/image_rectangle
@@ -27,11 +28,10 @@ roslaunch turtlebot3_slam turtlebot3_slam.launch slam_methods:=frontier_explorat
 roslaunch explore_lite explore.launch
 """
 
-bridge = CvBridge()
 
 class Distance_estimation():
 	def __init__(self):
-		rospy.init_node("image_converter", anonymous=True)
+		rospy.init_node("distance_estimation", anonymous=True)
 		self.bridge = CvBridge()
 		#for turtlebot3 waffle
 		#image_topic = '/camera/rgb/image_raw/compressed'
@@ -113,98 +113,20 @@ class Distance_estimation():
 			#if(self.current_time - self.passed_time > rospy.Duration(1)):
 			#TODO Define for each objects
 			self.draw_rectangle_on_roi(object_found)
-
-			if (object_found.data[0] == self.image_person):
-				#Add a color detector with openCV
-				state = self.detect_people_state()
-				if(state == self.alive):
-					print('Alive person found')
-					self.createMarker(Shape.SPHERE, 0.0, 1.0, 0.0)
-				else:
-					print('Injured person found')
-					self.createMarker(Shape.SPHERE, 1.0, 0.0, 0.0)
-				self.markerPub.publish(self.robotMarker)
-				self.count = self.count + 1
-				self.passed_time = rospy.get_rostime()
-
-			if (object_found.data[0] == self.image_toxic):
-				#Add a color detector with openCV
-				print('Toxic area found')
-				self.createMarker(Shape.SPHERE, 0.0, 0.0, 0.0)
-				self.markerPub.publish(self.robotMarker)
-				self.count = self.count + 1
-				self.passed_time = rospy.get_rostime()
-
-			if (object_found.data[0] == self.image_warning):
-				#Add a color detector with openCV
-				print('Warning are found')
-				self.createMarker(Shape.CUBE, 1.0, 0.0, 0.0)
-				self.markerPub.publish(self.robotMarker)
-				self.count = self.count + 1
-				self.passed_time = rospy.get_rostime()
-			"""
-			if (object_found.data[0] == self.image_injured):
-				#Add a color detector with openCV
-				print('Injured person found')
-				self.createMarker(Shape.SPHERE, 1.0, 0.0, 0.0)
-				self.markerPub.publish(self.robotMarker)
-				self.count = self.count + 1
-				self.passed_time = rospy.get_rostime()
-			"""
-			if (object_found.data[0] == self.image_fire):
-				#Add a color detector with openCV
-				print('Fire found')
-				self.createMarker(Shape.CYLINDER, 1.0, 0.0, 0.0)
-				self.markerPub.publish(self.robotMarker)
-				self.count = self.count + 1
-				self.passed_time = rospy.get_rostime()
-
-			if (object_found.data[0] == self.image_no_smoke):
-				#Add a color detector with openCV
-				print('Radioactive area found')
-				self.createMarker(Shape.CUBE, 0.5, 0.5, 0.5)
-				self.markerPub.publish(self.robotMarker)
-				self.count = self.count + 1
-				self.passed_time = rospy.get_rostime()
-
-			if (object_found.data[0] == self.image_radioactive):
-				#Add a color detector with openCV
-				print('Radioactive area found')
-				self.createMarker(Shape.CUBE, 1.0, 1.0, 0.0)
-				self.markerPub.publish(self.robotMarker)
-				self.count = self.count + 1
-				self.passed_time = rospy.get_rostime()
-
-			if (object_found.data[0] == self.image_dead):
-				#Add a color detector with openCV
-				print('Object 14 found')
-				self.createMarker(Shape.CUBE, 1.0, 0.0, 0.0)
-				self.markerPub.publish(self.robotMarker)
-				self.count = self.count + 1
-				self.passed_time = rospy.get_rostime()
-
-			if (object_found.data[0] == self.image_dead):
-				#Add a color detector with openCV
-				print('Object 14 found')
-				self.createMarker(Shape.SPHERE, 0.0, 0.0, 0.0)
-				self.markerPub.publish(self.robotMarker)
-				self.count = self.count + 1
-				self.passed_time = rospy.get_rostime()
-
-			else:
-				print("No object found")
-			
 		else:
-			print("Object to far to get the distance")
+			self.img.publish(self.actual_image)
+
 
 	def draw_rectangle_on_roi(self, object_found):
 		data = object_found.data
 		focal_length_mm = 3.04
 		sensor_width_mm = 3.68
-		image_width_px = 1920
+		#image_width_px = 1920
+		image_width_px = 640
 		focal_length_px = (focal_length_mm /sensor_width_mm) * image_width_px
 
-		camera_angle_conv = 0.0334
+		#camera_angle_conv = 0.0334
+		camera_angle_conv = 0.10366
 		camera_min_angle = -31.1
 		camera_max_angle = 31.1
 
@@ -230,25 +152,102 @@ class Distance_estimation():
 				height = abs(qtBottomRight.y() - qtTopRight.y())
 				ximage = qtBottomRight.x() - (qtBottomRight.x() - qtBottomLeft.x()) / 2
 				yimage = qtBottomRight.y() - (qtBottomRight.y() - qtTopRight.y()) / 2
+				distance = (focal_length_px * object_size) / height
+				self.object_distance = distance
+				angle_deg = int(camera_min_angle + camera_angle_conv * ximage)
+				self.object_angle = angle_deg
+				# Draw the rectangle around the object
+				font = cv2.FONT_HERSHEY_SIMPLEX
+				cv2.rectangle(cv_image, (int(qtTopLeft.x()), int(qtTopLeft.y())), (int(qtBottomRight.x()), int(qtBottomRight.y())), (0, 255, 0), 2)
+				cv2.putText(cv_image, str(round(distance, 2)) + "m and " + str(round(angle_deg, 2)) + "degree", (int(qtTopLeft.x()), int(qtTopLeft.y())), font, 2, (255, 0, 255), 2)
+				#cv2.putText(cv_image, str(round(distance, 2)) + "m", (int(qtTopLeft.x()), int(qtTopLeft.y())), font, 2, (255, 0, 255), 2)
+				image_message = bridge.cv2_to_imgmsg(cv_image, "bgr8")
+				print(distance)
+				
+				if (object_found.data[i] == self.image_person):
+					#Add a color detector with openCV
+					state = self.detect_people_state()
+					if(state == self.alive):
+						print('Alive person found')
+						self.createMarker(Shape.SPHERE, 0.0, 1.0, 0.0)
+					else:
+						print('Injured person found')
+						self.createMarker(Shape.SPHERE, 1.0, 0.0, 0.0)
+					self.markerPub.publish(self.robotMarker)
+					self.count = self.count + 1
+					self.passed_time = rospy.get_rostime()
 
-			distance = (focal_length_px * object_size) / height
-			self.object_distance = distance
-			angle_deg = int(camera_min_angle + camera_angle_conv * ximage)
-			self.object_angle = angle_deg
-			# Draw the rectangle around the object
-			font = cv2.FONT_HERSHEY_SIMPLEX
-			cv2.rectangle(cv_image, (int(qtTopLeft.x()), int(qtTopLeft.y())), (int(qtBottomRight.x()), int(qtBottomRight.y())), (0, 255, 0), 2)
-			cv2.putText(cv_image, str(round(distance, 2)) + "m and " + str(round(angle_deg, 2)) + "degree", (int(qtTopLeft.x()), int(qtTopLeft.y())), font, 2, (255, 0, 255), 2)
-			#cv2.putText(cv_image, str(round(distance, 2)) + "m", (int(qtTopLeft.x()), int(qtTopLeft.y())), font, 2, (255, 0, 255), 2)
-			image_message = bridge.cv2_to_imgmsg(cv_image, "bgr8")
-			print(distance)
-			
-			
+				if (object_found.data[i] == self.image_toxic):
+					#Add a color detector with openCV
+					print('Toxic area found')
+					self.createMarker(Shape.SPHERE, 0.0, 0.0, 0.0)
+					self.markerPub.publish(self.robotMarker)
+					self.count = self.count + 1
+					self.passed_time = rospy.get_rostime()
+
+				if (object_found.data[i] == self.image_warning):
+					#Add a color detector with openCV
+					print('Warning are found')
+					self.createMarker(Shape.CUBE, 1.0, 0.0, 0.0)
+					self.markerPub.publish(self.robotMarker)
+					self.count = self.count + 1
+					self.passed_time = rospy.get_rostime()
+				"""
+				if (object_found.data[0] == self.image_injured):
+					#Add a color detector with openCV
+					print('Injured person found')
+					self.createMarker(Shape.SPHERE, 1.0, 0.0, 0.0)
+					self.markerPub.publish(self.robotMarker)
+					self.count = self.count + 1
+					self.passed_time = rospy.get_rostime()
+				"""
+				if (object_found.data[i] == self.image_fire):
+					#Add a color detector with openCV
+					print('Fire found')
+					self.createMarker(Shape.CYLINDER, 1.0, 0.0, 0.0)
+					self.markerPub.publish(self.robotMarker)
+					self.count = self.count + 1
+					self.passed_time = rospy.get_rostime()
+
+				if (object_found.data[i] == self.image_no_smoke):
+					#Add a color detector with openCV
+					print('Radioactive area found')
+					self.createMarker(Shape.CUBE, 0.5, 0.5, 0.5)
+					self.markerPub.publish(self.robotMarker)
+					self.count = self.count + 1
+					self.passed_time = rospy.get_rostime()
+
+				if (object_found.data[i] == self.image_radioactive):
+					#Add a color detector with openCV
+					print('Radioactive area found')
+					self.createMarker(Shape.CUBE, 1.0, 1.0, 0.0)
+					self.markerPub.publish(self.robotMarker)
+					self.count = self.count + 1
+					self.passed_time = rospy.get_rostime()
+
+				if (object_found.data[i] == self.image_dead):
+					#Add a color detector with openCV
+					print('Object 14 found')
+					self.createMarker(Shape.CUBE, 1.0, 0.0, 0.0)
+					self.markerPub.publish(self.robotMarker)
+					self.count = self.count + 1
+					self.passed_time = rospy.get_rostime()
+
+				if (object_found.data[i] == self.image_dead):
+					#Add a color detector with openCV
+					print('Object 14 found')
+					self.createMarker(Shape.SPHERE, 0.0, 0.0, 0.0)
+					self.markerPub.publish(self.robotMarker)
+					self.count = self.count + 1
+					self.passed_time = rospy.get_rostime()
+
+				else:
+					print("No object found")
 			self.img.publish(image_message)
 			
 		else:
 			print("Nothing detected")
-			self.img.publish(self.actual_image)
+			
 
 	def createMarker(self, shape, color_r, color_g, color_b):
 		self.robotMarker = Marker()
@@ -293,8 +292,8 @@ class Distance_estimation():
 		greenLower = (40, 40,40)
 		greenUpper = (70, 255,255)
 		greenmask = cv2.inRange(hsv, greenLower, greenUpper)
-		cv2.imshow("Green Mask", greenmask)
-		cv2.waitKey(1)
+		#cv2.imshow("Green Mask", greenmask)
+		#cv2.waitKey(1)
 
 		_, contours, _ = cv2.findContours(greenmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 		if len(contours) > 0:
---
+++ src/generate_image_from_odom_occupancy.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+import numpy as np
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from nav_msgs.msg import OccupancyGrid, Odometry
+from cv_bridge import CvBridge, CvBridgeError
+import sys
+bridge = CvBridge()
+
+class ROS_image_occupancy_creator():
+	def __init__(self):
+		rospy.init_node("image_converter", anonymous=True)
+		self.bridge = CvBridge()
+		#for turtlebot3 waffle
+		#image_topic = '/camera/rgb/image_raw/compressed'
+		#for occupancy topic
+		self.image_topic = '/map'
+		self.resolution = 0.05
+		self.rectangle_odom = 5
+		#image_topic = '/camera/rgb/image_raw/'
+		self.robot_odom_value = Odometry()
+		self.image_sub = rospy.Subscriber(self.image_topic, OccupancyGrid, self.occupancy_callback)
+		self.odom = rospy.Subscriber('/odom', Odometry, self.odom_callback)
+		self.img = rospy.Publisher('image_from_occupancy', Image, queue_size=10)
+		self.image_message = Image()
+		#self.image = np.zeros((384, 384, 1), np.uint8)
+		self.image = np.zeros((1920, 1080, 1), np.uint8)
+		self.count = 0
+	
+
+	def occupancy_callback(self, ros_occupancy):
+
+		print 'got an OccupancyGrid'
+		image = np.zeros((1920, 1080, 1), np.uint8)
+		self.occupancy = ros_occupancy.data
+		raw_data = np.reshape(ros_occupancy.data, (-1, 384))
+		#print (raw_data)
+		#cv2.imshow("Image Panel", image)
+		# convert ros_occupancy into an opencv-compatible image
+		for raw in range(0, 384):
+			for cols in range(0,384):
+				if(raw_data[raw, cols] == -1):
+					image[raw, cols] = 125
+				if(raw_data[raw, cols] == 100):
+					image[raw, cols] = 0
+				if(raw_data[raw, cols] == 0):
+					image[raw, cols] = 255
+
+		self.image = image
+		
+		#intermedate_image.publish(image_message)
+
+	def odom_callback(self, ros_odom):
+		
+		image_to_send = np.zeros((384, 384, 1), np.uint8)
+		
+		image_to_send = self.image.copy() # without .copy mthod it write directly on self.image
+		self.robot_odom_value = ros_odom
+		odom_pose_x = int((self.robot_odom_value.pose.pose.position.x + 10)/(self.resolution))
+		odom_pose_y = int((self.robot_odom_value.pose.pose.position.y + 10)/(self.resolution))
+		print("The calculated value is " + str(odom_pose_x) +" and "+ str(odom_pose_y))
+		cv2.rectangle(image_to_send, (odom_pose_x - 5, odom_pose_y - 5), (odom_pose_x + 5, odom_pose_y + 5), (0, 255, 0), 2)
+		image_to_send = cv2.rotate(image_to_send, cv2.ROTATE_90_CLOCKWISE)
+		image_to_send = cv2.flip(image_to_send, 0)
+		#image_to_send = cv2.rotate(image_to_send, cv2.ROTATE_180)
+		image_message = bridge.cv2_to_imgmsg(image_to_send, "mono8")
+		
+		self.img.publish(image_message)
+		
+
+def main(args):
+	ROS_image_occupancy_creator()
+	try:
+		rospy.spin()
+	except rospy.KeyboardInterrupt:
+		print("Shutting down")
+	cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+	main(sys.argv)
